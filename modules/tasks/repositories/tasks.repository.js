f9d44db42c9fa16d320de11ed56c9540720101d9
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2026-04-09 07:55:02 +0200

    fix(tasks): update JSDoc param type for update() to reflect Mongoose document requirement

--- modules/tasks/repositories/tasks.repository.js
+++ modules/tasks/repositories/tasks.repository.js
@@ -42,7 +42,7 @@ const get = (id) => {
 /**
  * @function update
  * @description Data access operation to update an existing task in the database.
- * @param {Object} task - The task object containing the updated details.
+ * @param {import('mongoose').Document} task - The hydrated Mongoose Task document to update. Must support `.save()`.
  * @returns {Object} The updated task.
  */
 const update = (task) => task.save().then((doc) => doc.populate(defaultPopulate));
